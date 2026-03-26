aa3725030403c66e598ec4860c289dbf629fa247
antvx <antocara4@outlook.it>
2026-03-26 17:54:58 +0100

    resi effettivi/realistici i contatori nella home-page

--- src/app/home-page/home-page.ts
+++ src/app/home-page/home-page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { LineService } from '../line-service';
+import { LineaTrasporto } from '../model/entities';
 
 @Component({
   selector: 'app-home-page',
@@ -7,18 +9,32 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
-export class HomePage
+export class HomePage implements OnInit
 {
-  // Potresti aggiungere statistiche rapide qui in futuro
-  stats = {
-    activeLines: 12,
-    totalStops: 145,
-    driversOnDuty: 8
-  };
+  activeLinesCount: number = 0;
+  totalStopsCount: number = 0;
+  driversCount: number = 8; // Per ora lo teniamo fisso a 8 o lo calcoliamo se hai una lista autisti
+
+  constructor(private lineService: LineService) {}
+
+  ngOnInit() {
+    this.updateStats();
+  }
+
+  updateStats() {
+    const allLines: any[] = this.lineService.getLines();
+
+    // 1. Conteggio Linee Attive
+    this.activeLinesCount = allLines.length;
+
+    // 2. Conteggio Fermate Totali (Risoluzione errore TS)
+    // Specifichiamo che acc è un number e line è any (o LineaTrasporto)
+    this.totalStopsCount = allLines.reduce((acc: number, line: any) => {
+      return acc + (line.stops ? line.stops.length : 0);
+    }, 0);
+
+    // 3. Autisti in Servizio
+    // Possiamo renderlo dinamico basandoci sulle linee (es. 2 autisti per ogni linea attiva)
+    this.driversCount = this.activeLinesCount * 2;
+  }
 }
-
-
-
-
-
-
